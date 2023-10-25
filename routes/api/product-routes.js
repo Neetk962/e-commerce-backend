fc56508d25f6ffc44def30607d6fad006c22461e
Ravneet Kaur <ravneet.kaur9629@gmail.com>
2023-10-25 12:52:04 -0700

    //added category & product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
+  const products = await Product.findAll();
+  res.json(products)
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+
+  const product = await Product.findOne({ where:
+     { id: req.params.id }, include: Category, Tag
+     });
+     res.json(product)
+
+  // included its associated Category and Tag data
 });
 
 // create new product
